3bc2ae64e73ca80f8f8123af91003b52e3d1cae9
sm6878 <shubham.mishra6878@gmail.com>
2021-06-11 00:21:45 +0530

    Modular Exponentiation using Repeated Squaring Paradigm

---
+++ Modular Exponentiation.cpp
@@ -0,0 +1,22 @@
+//Modular Exponentiation using Repeated Squaring Paradigm
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int power(long int x,long int y,long int m)
+{
+    if(y==0)
+    {
+        return 1;
+    }
+    int k=power(x,y/2,m);
+    if(y%2==1)
+    {
+        return ((((k*k)%m)*x)%m);
+    }
+    else
+        return ((k*k)%m);
+}
+
+//Time Complexity : O(1)
+//Space Complexity : O(log(n))
